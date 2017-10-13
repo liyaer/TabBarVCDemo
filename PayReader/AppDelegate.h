ed1e27c4eeb221d82849b85570357aea3ceca99a
DuWenLiang <739285514@qq.com>
2017-10-13 11:11:24 +0800

    Initial Commit

---
+++ PayReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PayReader
+//
+//  Created by 杜文亮 on 2017/10/13.
+//  Copyright © 2017年 杜文亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
