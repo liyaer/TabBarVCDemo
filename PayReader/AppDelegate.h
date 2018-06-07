c1166ce48d9320674470cd02181713931d231db0
Poker Face <739285514@qq.com>
2018-06-07 16:26:07 +0800

    Update AppDelegate.h

--- PayReader/AppDelegate.h
+++ PayReader/AppDelegate.h
@@ -10,7 +10,7 @@
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-//如果入口设置删除了Main,那么使用window属性时需要初始化；这里我们从Main.storyBoard加载，所以不用初始化window
+//如果入口设置删除了Main,那么使用window属性时需要初始化，并且最后调用 [self.window makeKeyAndVisible]；这里我们从Main.storyBoard加载，所以不用初始化window
 @property (strong, nonatomic) UIWindow *window;
 
 @end
